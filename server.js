c0aadc0956a90adbaf6a03872a72055843f93b15
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-04-12 15:30:24 -0400

    dev formatting set up

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+require("./src/config/connection");
+
 const app = express();
 
 app.get("/testing", (req, res) => {
   res.send("Hello World");
 });
+
+app.use(express.json());
+app.use(cors());
