dcf32d3c03f485f0c535d65275294000fd6477db
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-05-02 13:14:15 -0400

    created properties for userScheme

--- src/resources/admin/admin.model.js
+++ src/resources/admin/admin.model.js
@@ -3,6 +3,36 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: (value) => {
+      if (!validator.isEmail(value)) {
+        throw new Error({ error: "Invalid Email address" });
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
 
 module.exports = User;
