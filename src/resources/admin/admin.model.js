af78ee912beb0b75cb2fc15f706014282a699751
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-05-02 13:37:25 -0400

    functions for authentication

--- src/resources/admin/admin.model.js
+++ src/resources/admin/admin.model.js
@@ -35,4 +35,40 @@ const userSchema = mongoose.Schema({
   ],
 });
 
+// hash password with bcrypt
+userSchema.pre("save", async function (next) {
+  // Hash the password before saving the user model
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+// add token to user model
+userSchema.methods.generateAuthToken = async function () {
+  // Generate an auth token for the user
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+// compare password with bcrypt and we would use this in the signin authe
+userSchema.statics.findByCredentials = async (email, password) => {
+  // Search for a user by email and password.
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error({ error: "Invalid login credentials" });
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    throw new Error({ error: "Invalid login credentials" });
+  }
+  return user;
+};
+
+const User = mongoose.model("User", userSchema);
+
 module.exports = User;
