451912369cf61112ef0954061ac8a86fde1141be
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-05-12 09:18:13 -0400

    created user model

--- src/resources/user/user.model.js
+++ src/resources/user/user.model.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: String, required: true, default: false },
+});
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = Cart;
+module.exports = User;
